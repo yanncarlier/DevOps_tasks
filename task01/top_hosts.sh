1596786cc1856a810efd4d08f70c5b8230ce4535
Yann Carlier <carlier@gmail.com>
2023-09-22 14:33:16 +0700

    this needs revision (date format) / qa testing / before production

--- task01/top_hosts.sh
+++ task01/top_hosts.sh
@@ -7,6 +7,9 @@ start_date="09/Jul/2019:00:00:00"
 end_date="19/Jul/2019:23:59:59"
 
 # Use awk to filter log entries within the date range and count requests per host
+############
+# this needs revision (date format) / qa testing / before production 
+############
 requests=$(awk -v start="$start_date" -v end="$end_date" '$4 >= "["start"]" && $4 <= "["end"]" {print $1}' "$logfile" | sort | uniq -c | sort -nr)
 
 # Display the top 10 hosts with the most requests
